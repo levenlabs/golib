d5f76c578c57dc8e080a1942e47b321e0501a20a
Brian Picciano <mediocregopher@gmail.com>
2015-09-17 14:34:31 -0600

    update some documentation

--- rpcutil/server.go
+++ rpcutil/server.go
@@ -7,7 +7,8 @@ import (
 )
 
 // JSONRPC2Handler returns an http.Handler which will handle JSON RPC2 calls
-// directed at the given services
+// directed at the given services. A service is a type that has a set of RPC
+// methods on it which will be discovered and used by the rpc package.
 func JSONRPC2Handler(codec rpc.Codec, service ...interface{}) http.Handler {
 	s := rpc.NewServer()
 	s.RegisterCodec(codec, "application/json")
--- testutil/testutil.go
+++ testutil/testutil.go
@@ -18,8 +18,8 @@ func RandStr() string {
 	return hex.EncodeToString(b)
 }
 
-// RandInt64 returns a random integer. greater than or equal to 0. It's not
-// necessary to do any seeding to use this method
+// RandInt64 returns a random integer. greater than or equal to 0, and less than
+// 1 million. It's not necessary to do any seeding to use this method
 func RandInt64() int64 {
 	i, err := rand.Int(rand.Reader, big.NewInt(1000000))
 	if err != nil {
