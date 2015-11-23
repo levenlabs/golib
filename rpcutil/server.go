8ec0840e6a9736128dfa0b215978155703e6a817
Brian Picciano <mediocregopher@gmail.com>
2015-11-23 14:06:50 -0700

    use gatewayrpc as default rpc server implementation

--- rpcutil/server.go
+++ rpcutil/server.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gorilla/rpc/v2"
+	"github.com/levenlabs/gatewayrpc"
 )
 
 // JSONRPC2Handler returns an http.Handler which will handle JSON RPC2 calls
 // directed at the given services. A service is a type that has a set of RPC
 // methods on it which will be discovered and used by the rpc package.
 func JSONRPC2Handler(codec rpc.Codec, service ...interface{}) http.Handler {
-	s := rpc.NewServer()
+	s := gatewayrpc.NewServer()
 	s.RegisterCodec(codec, "application/json")
 	for i := range service {
 		s.RegisterService(service[i], "")
