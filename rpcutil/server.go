2055895938fff1fd7263f78ef9c18006f6871b34
Brian Picciano <mediocregopher@gmail.com>
2016-05-18 11:49:47 -0600

    don't use gatewayrpc in rpcutil, whoever thought that was a good idea was clearly an idiot

--- rpcutil/server.go
+++ rpcutil/server.go
@@ -5,15 +5,16 @@ import (
 	"reflect"
 
 	"github.com/gorilla/rpc/v2"
-	"github.com/levenlabs/gatewayrpc"
 	"github.com/levenlabs/go-llog"
 )
 
 // JSONRPC2Handler returns an http.Handler which will handle JSON RPC2 calls
 // directed at the given services. A service is a type that has a set of RPC
 // methods on it which will be discovered and used by the rpc package.
+//
+// This method is super deprecated and very dumb, don't use it
 func JSONRPC2Handler(codec rpc.Codec, service ...interface{}) http.Handler {
-	s := gatewayrpc.NewServer()
+	s := rpc.NewServer()
 	s.RegisterCodec(codec, "application/json")
 	for i := range service {
 		if err := s.RegisterService(service[i], ""); err != nil {
