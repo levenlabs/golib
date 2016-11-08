b34081ca46d75e161325c43934a0a830b031bfcc
Brian Picciano <mediocregopher@gmail.com>
2016-11-08 06:33:38 -0700

    make private-cidrs be settable no matter what mode

--- genapi/genapi.go
+++ genapi/genapi.go
@@ -842,6 +842,11 @@ func (g *GenAPI) doLever() {
 		Description:  "[address]:port to listen for requests on. If port is zero a port will be chosen randomly",
 		DefaultMulti: []string{":0"},
 	})
+	g.Lever.Add(lever.Param{
+		Name:        "--private-cidrs",
+		Description: "Comma separated list of cidrs which are considered to be our private network",
+		Default:     "127.0.0.1/32,::1/128,10.0.0.0/8",
+	})
 	if g.TLSInfo != nil {
 		g.Lever.Add(lever.Param{
 			Name:         "--tls-listen-addr",
@@ -869,11 +874,6 @@ func (g *GenAPI) doLever() {
 		g.Lever.Add(lever.Param{
 			Name:        "--skyapi-addr",
 			Description: "Hostname of skyapi, to be looked up via a SRV request. Unset means don't register with skyapi",
-		})
-		g.Lever.Add(lever.Param{
-			Name:        "--private-cidrs",
-			Description: "Comma separated list of cidrs which are considered to be our private network",
-			Default:     "127.0.0.1/32,::1/128,10.0.0.0/8",
 		})
 		g.Lever.Add(lever.Param{
 			Name:        "--unhealthy-timeout",
