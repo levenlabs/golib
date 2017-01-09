8126e9d6f4444d674d3cb0649ca9fda7a293bf43
Brian Picciano <mediocregopher@gmail.com>
2017-01-09 09:07:30 -0700

    allow genapi.RetryCaller to retry infinitely

--- genapi/genapi.go
+++ genapi/genapi.go
@@ -1244,16 +1244,19 @@ type retryCaller struct {
 // back to it. If any return any errors they will be retried the given number of
 // times until one doesn't return an error. The most recent error is returned if
 // all attempts fail.
+//
+// If attempts is -1 then RetryCaller will never give up
 func RetryCaller(c Caller, attempts int) Caller {
 	return retryCaller{c, attempts}
 }
 
 func (rc retryCaller) Call(ctx context.Context, res interface{}, method string, args interface{}) error {
 	var err error
-	for i := 0; i < rc.attempts; i++ {
+	for i := 0; i < rc.attempts || rc.attempts < 0; i++ {
 		if err = rc.Caller.Call(ctx, res, method, args); err == nil {
 			return nil
 		}
+		time.Sleep(50 * time.Millisecond)
 	}
 	return err
 }
